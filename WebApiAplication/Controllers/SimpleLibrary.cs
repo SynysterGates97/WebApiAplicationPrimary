af94ff5694ae6eec2a2e58988c5eb00b7313d33e
One-X-Two <rusllann97@gmail.com>
2020-11-25 22:59:06 +0500

    Переименовал основной контроллер. Добавил обработку post запроса со строкой в теле

--- WebApiAplication/Controllers/SimpleLibrary.cs
+++ WebApiAplication/Controllers/SimpleLibrary.cs
@@ -5,40 +5,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApiAplication.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace WebApiAplication.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : Controller
+    public class SimpleLibrary : ControllerBase
     {
         public AppDataBaseContext _ctx;
 
-        public WeatherForecastController(AppDataBaseContext ctx)
+        public SimpleLibrary(AppDataBaseContext ctx)
         {
             _ctx = ctx;
         }
 
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
         [HttpPost]
-        public string Post([FromBody] string value)
+        public void Post2([FromBody] string name)
         {
             Dweller newUser = new Dweller();
             newUser.HP = 100;
-            newUser.Name = "Одинокий путник";
+            newUser.Name = name;
 
             _ctx.Dwellers.Add(newUser);
             _ctx.SaveChanges();
-            return "Не унывайте, пацаны";
         }
+
+        //[HttpPost]
+        //public void Post()
+        //{
+        //    Dweller newUser = new Dweller();
+        //    newUser.HP = 100;
+        //    newUser.Name = "STD";
+
+        //    _ctx.Dwellers.Add(newUser);
+        //    _ctx.SaveChanges();
+        //}
+
 
         [HttpGet]
         public string Get()
         {
             Dweller newUser = new Dweller();
-            //newUser.HP = 100;
+            newUser.HP = 100;
             newUser.Name = "Одинокий путник";
 
             _ctx.Dwellers.Add(newUser);
